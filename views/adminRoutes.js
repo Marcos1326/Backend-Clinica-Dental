5ccf9cb44340b3f14de0dbafab5bb74f4ecec5d7
Marcos <marcosmarquitos132@gmail.com>
2023-03-07 22:52:08 +0100

    feat: creacion del endpoint getAllRol (admin)

--- views/adminRoutes.js
+++ views/adminRoutes.js
@@ -9,5 +9,6 @@ router.get("/adminappointments", verifyToken, isAdmin, adminController.getAppoin
 router.post("/admin-createrol", verifyToken, isAdmin, adminController.createRol)
 router.post("/admin-updaterol", verifyToken, isAdmin, adminController.updateRol)
 router.post("/admin-deleterol", verifyToken, isAdmin, adminController.deleteRol)
+router.post("/admin-getallrol", verifyToken, isAdmin, adminController.getAllRol)
 
 module.exports = router;
