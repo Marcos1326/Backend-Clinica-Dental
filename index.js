bf0d54b38acccc0722be81d5a6d872d6eb2501a9
David Soto Sousa <sousavlc86@gmail.com>
2023-03-17 13:50:31 +0100

    Instalar dependencia cors

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 const db = require('./db/db');
 const app = express();
 const PORT = process.env.PORT || 4000;
+const cors = require('cors')
 app.use(express.json());
 
 const router = require("./router")
